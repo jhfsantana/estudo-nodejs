7a394f1a6a9a98c9fa1ca2463ef19f124633597f
Jorge Henrique <jorge.santana@ubook.com>
2017-11-13 16:14:05 -0200

    adicionando validacao, criando resposta generica json ou html

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -5,19 +5,45 @@ module.exports = function(app) {
 		dao = new app.infra.ProdutosDAO(connectionFactory);
 
 		dao.lista(function(err, results){
-			response.render('produtos/lista', {produtos: results});
+			response.format({
+				html: function() {
+					response.render('produtos/lista', {produtos: results});
+				},
+				json: function() {
+					response.json(results);
+				}
+			});
 		});
+
 		connectionFactory.end();
 	});
 
 	app.get('/produtos/novo', function(request, response){
-		response.render('produtos/formulario');
+		response.render('produtos/formulario',{errors: '', produto: ''});
 	});
 
 	app.post('/produtos', function(request, response){
 		var produto = request.body;
 		connectionFactory = app.infra.connectionFactory();
 		dao = new app.infra.ProdutosDAO(connectionFactory);
+
+		request.assert('titulo', 'Campo titulo e obrigatório!').notEmpty();
+		request.assert('descricao', 'Campo titulo e descricao!').notEmpty();
+
+		var errors = request.validationErrors();
+
+		if(errors) {
+			response.format({
+				html: function() {
+					response.status(400).render('produtos/formulario', {errors: errors, produto: produto});
+				},
+				json: function() {
+					response.status(400).json(errors);
+				}
+			});
+			return;
+		}
+
 		dao.salvar(produto, function(err, results){
 			console.log(err);
 			response.redirect('produtos');
